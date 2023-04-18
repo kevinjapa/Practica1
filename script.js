13ca927869cf43ecdb3b38bf7505150f2c6ed7d9
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2023-04-17 23:49:20 -0500

    email

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hello!');
+function validarCorreoElectronico(correo) {
+    var expresionRegular = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return expresionRegular.test(correo);
+  }
+  
+  // Ejemplo de uso
+function correo()
+{
+  var correoElectronico = document.getElementById("email");
+  if (validarCorreoElectronico(correoElectronico)) {
+    alert("Notificacion enviada...");
+    console.log(good)
+  } else {
+    alert("Correo Incorrecto");
+  }
+}
+  
